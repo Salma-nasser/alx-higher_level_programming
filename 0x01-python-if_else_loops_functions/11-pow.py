e477bc3b3142267443764a3581f852557f6bb9e8
Salma Nasser <salma.naser1020@gmail.com>
2024-01-16 14:18:47 -0800

    12

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    else:
+        result = 1.0
+        for _ in range(abs(b)):
+            result /= a
+        return result
