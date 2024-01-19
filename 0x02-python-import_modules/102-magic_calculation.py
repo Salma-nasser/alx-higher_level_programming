98b7a56a5211c01ba60afca9771613efb2dbeb24
Salma Nasser <salma.naser1020@gmail.com>
2024-01-18 18:05:36 -0800

    salma

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
-    if a < b:
+    """Perform magic calculations"""
+
+    from magic_calculation_102 import add, sub
+
+    if (a < b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+        return (c)
 
-        return c
-
-    return sub(a, b)
+    else:
+        return (sub(a, b))
--- 0x02-python-import_modules/main.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-magic_calculation = 102-magic_calculation.magic_calculation
-magic_calculation(3, 5)
