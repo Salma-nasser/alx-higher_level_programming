4fb97e2af4fcfe3770ded48be38fd9fcd77039c6
Salma Nasser <salma.naser1020@gmail.com>
2024-01-24 10:38:59 -0800

    sa;\o\louma is finished

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """Delete keys with a specific value in a dictionary"""
+
+    if a_dictionary is None:
+        return None
+    keys_to_delete = [k for k, v in a_dictionary.items() if v == value]
+    for key in keys_to_delete:
+        del a_dictionary[key]
+    return a_dictionary
