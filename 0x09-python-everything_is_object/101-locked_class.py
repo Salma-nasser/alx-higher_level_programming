69ae7389d43effa9a455a2eb0583c32be2585aca
Salma Nasser <salma.naser1020@gmail.com>
2024-02-14 08:41:37 -0800

    salma

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,6 +7,8 @@ that prevents user: from creating new instance attribute
 except: if the new instance attribute is called first_name
 
 """
+
+
 class LockedClass:
     """ A class that prevent instance and only allow instance
     with the attribute: first_name
