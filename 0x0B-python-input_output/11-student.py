4a9ee1b82f29603a210d46a84e67a57e53a70785
Salma Nasser <salma.naser1020@gmail.com>
2024-02-20 14:40:35 -0800

    s

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
-"""
-Defines Student class
-"""
+""" a class that defines a student based on 9-student.py"""
 
 
-class Student():
-    """
-        A Student class
-    """
+class Student:
+    """ A Student class """
     def __init__(self, first_name, last_name, age):
-        """
-            defines first_name, last_name, age
-        """
+        """ the instantiation """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-            retrieves a dictionary representation
-            of a Student instance
-        """
-        if type(attrs) is list and all([type(s) == str for s in attrs]):
-            return {i: j for i, j in self.__dict__.items() if i in attrs}
-        return(self.__dict__)
+        """ a json function that retrieves a dic.. rep of student """
+        if type(attrs) is list and all(type(item) is str for item in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """
-            reload json object
-        """
-        for i, j in json.items():
-            self.__dict__[i] = j
+        """ a public method that replaces all attributes of d stud.."""
+        for key, value in json.items():
+            setattr(self, key, value)
