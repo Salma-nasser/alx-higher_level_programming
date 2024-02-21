0cc5c7bdeb708c86cf9f413c313976430bae19f4
Salma Nasser <salma.naser1020@gmail.com>
2024-02-21 13:29:05 -0800

    advanceeed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+""" A function that returns the list of available attributes
+and methods of an object"""
+
+
 def lookup(obj):
-    """returns possible attributes
-    """
+    """ The function that takes object as an argument"""
     return dir(obj)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-returns True if the object is exactly an instance
-of the specified class ;otherwise False
-"""
+""" a function that returns true if the obj is specified class"""
 
 
 def is_same_class(obj, a_class):
-    """implementation
-    Args:
-        obj (Any): object to check
-        a_class (type): type to check against
-    Returns:
-        [boolean]: response
-    """
-    return type(obj) == a_class
+    """ checks if the object is the same class"""
+    if type(obj) is a_class:
+        return True
+    return False
